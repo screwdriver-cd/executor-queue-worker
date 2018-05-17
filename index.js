eb40193b414fbac611af155ca7f2303d508df518
Dao Lam <daolam112@gmail.com>
2018-05-17 10:54:44 -0700

    address comments

--- index.js
+++ index.js
@@ -55,20 +55,20 @@ function updateBuildStatus(updateConfig, callback) {
  * @param  {Object}       worker        worker to be ended
  * @param  {Object}       scheduler     scheduler to be ended
  */
-function shutDownAll(worker, scheduler) {
-    worker.end((error) => {
-        if (error) {
-            winston.error(`failed to end the worker: ${error}`);
-        }
+async function shutDownAll(worker, scheduler) {
+    try {
+        await worker.end();
+    } catch (error) {
+        winston.error(`failed to end the worker: ${error}`);
+    }
 
-        scheduler.end((err) => {
-            if (err) {
-                winston.error(`failed to end the scheduler: ${err}`);
-                process.exit(128);
-            }
-            process.exit(0);
-        });
-    });
+    try {
+        await scheduler.end();
+        process.exit(0);
+    } catch (err) {
+        winston.error(`failed to end the scheduler: ${err}`);
+        process.exit(128);
+    }
 }
 
 const supportFunction = { updateBuildStatus, shutDownAll };
@@ -86,56 +86,56 @@ const multiWorker = new NodeResque.MultiWorker({
 
 const scheduler = new NodeResque.Scheduler({ connection: connectionDetails });
 
-multiWorker.on('start', workerId =>
-    winston.info(`worker[${workerId}] started`));
-multiWorker.on('end', workerId =>
-    winston.info(`worker[${workerId}] ended`));
-multiWorker.on('cleaning_worker', (workerId, worker, pid) =>
-    winston.info(`cleaning old worker ${worker} pid ${pid}`));
-multiWorker.on('poll', (workerId, queue) =>
-    winston.info(`worker[${workerId}] polling ${queue}`));
-multiWorker.on('job', (workerId, queue, job) =>
-    winston.info(`worker[${workerId}] working job ${queue} ${JSON.stringify(job)}`));
-multiWorker.on('reEnqueue', (workerId, queue, job, plugin) =>
-    winston.info(`worker[${workerId}] reEnqueue job (${plugin}) ${queue} ${JSON.stringify(job)}`));
-multiWorker.on('success', (workerId, queue, job, result) =>
-    winston.info(`worker[${workerId}] ${job} success ${queue} ${JSON.stringify(job)} >> ${result}`));
-multiWorker.on('failure', (workerId, queue, job, failure) =>
-    supportFunction.updateBuildStatus({ workerId, queue, job, failure }, () => {}));
-multiWorker.on('error', (workerId, queue, job, error) =>
-    winston.error(`worker[${workerId}] error ${queue} ${JSON.stringify(job)} >> ${error}`));
-multiWorker.on('pause', workerId =>
-    winston.info(`worker[${workerId}] paused`));
-
-// multiWorker emitters
-multiWorker.on('internalError', error =>
-    winston.error(error));
-multiWorker.on('multiWorkerAction', (verb, delay) =>
-    winston.info(`*** checked for worker status: ${verb} (event loop delay: ${delay}ms)`));
-
-scheduler.on('start', () =>
-    winston.info('scheduler started'));
-scheduler.on('end', () =>
-    winston.info('scheduler ended'));
-scheduler.on('poll', () =>
-    winston.info('scheduler polling'));
-scheduler.on('master', state =>
-    winston.info(`scheduler became master ${state}`));
-scheduler.on('error', error =>
-    winston.info(`scheduler error >> ${error}`));
-scheduler.on('working_timestamp', timestamp =>
-    winston.info(`scheduler working timestamp ${timestamp}`));
-scheduler.on('transferred_job', (timestamp, job) =>
-    winston.info(`scheduler enqueuing job timestamp  >>  ${JSON.stringify(job)}`));
-
-multiWorker.start();
-
 /**
  * Start scheduler
  * @method boot
  * @return {Promise}
  */
 async function boot() {
+    multiWorker.on('start', workerId =>
+        winston.info(`worker[${workerId}] started`));
+    multiWorker.on('end', workerId =>
+        winston.info(`worker[${workerId}] ended`));
+    multiWorker.on('cleaning_worker', (workerId, worker, pid) =>
+        winston.info(`cleaning old worker ${worker} pid ${pid}`));
+    multiWorker.on('poll', (workerId, queue) =>
+        winston.info(`worker[${workerId}] polling ${queue}`));
+    multiWorker.on('job', (workerId, queue, job) =>
+        winston.info(`worker[${workerId}] working job ${queue} ${JSON.stringify(job)}`));
+    multiWorker.on('reEnqueue', (workerId, queue, job, plugin) =>
+        winston.info(`worker[${workerId}] reEnqueue job (${plugin}) ${queue} ${JSON.stringify(job)}`));
+    multiWorker.on('success', (workerId, queue, job, result) =>
+        winston.info(`worker[${workerId}] ${job} success ${queue} ${JSON.stringify(job)} >> ${result}`));
+    multiWorker.on('failure', (workerId, queue, job, failure) =>
+        supportFunction.updateBuildStatus({ workerId, queue, job, failure }, () => {}));
+    multiWorker.on('error', (workerId, queue, job, error) =>
+        winston.error(`worker[${workerId}] error ${queue} ${JSON.stringify(job)} >> ${error}`));
+    multiWorker.on('pause', workerId =>
+        winston.info(`worker[${workerId}] paused`));
+
+    // multiWorker emitters
+    multiWorker.on('internalError', error =>
+        winston.error(error));
+    multiWorker.on('multiWorkerAction', (verb, delay) =>
+        winston.info(`*** checked for worker status: ${verb} (event loop delay: ${delay}ms)`));
+
+    scheduler.on('start', () =>
+        winston.info('scheduler started'));
+    scheduler.on('end', () =>
+        winston.info('scheduler ended'));
+    scheduler.on('poll', () =>
+        winston.info('scheduler polling'));
+    scheduler.on('master', state =>
+        winston.info(`scheduler became master ${state}`));
+    scheduler.on('error', error =>
+        winston.info(`scheduler error >> ${error}`));
+    scheduler.on('working_timestamp', timestamp =>
+        winston.info(`scheduler working timestamp ${timestamp}`));
+    scheduler.on('transferred_job', (timestamp, job) =>
+        winston.info(`scheduler enqueuing job timestamp  >>  ${JSON.stringify(job)}`));
+
+    multiWorker.start();
+
     await scheduler.connect();
     scheduler.start();
 }
