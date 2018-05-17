05dfc1d9f6001e61af149eb97cc17f5497cb39e4
Dao Lam <daolam112@gmail.com>
2018-05-17 10:30:40 -0700

    add test for blockedby

--- test/blockedBy.test.js
+++ test/blockedBy.test.js
@@ -1,191 +1,52 @@
-//
-// 'use strict';
-//
-// const path = require('path');
-// const specHelper = require(path.join(__dirname, '..', 'utils', 'specHelper.js'));
-// const NodeResque = require(path.join(__dirname, '..', '..', 'index.js'));
-//
-// let queue;
-// const jobDelay = 1000;
-// let worker1;
-// let worker2;
-//
-// const jobs = {
-//     slowAdd: {
-//         plugins: ['JobLock'],
-//         pluginOptions: { jobLock: {} },
-//         perform: async (a, b) => {
-//             const answer = a + b;
-//
-//             await new Promise((resolve) => { setTimeout(resolve, jobDelay); });
-//
-//             return answer;
-//         }
-//     }
-// };
-//
-// describe('plugins', () => {
-//     beforeAll(async () => {
-//         await specHelper.connect();
-//         await specHelper.cleanup();
-//         queue = new NodeResque.Queue({ connection: specHelper.cleanConnectionDetails(), queue: specHelper.queue }, jobs);
-//         await queue.connect();
-//     });
-//
-//     afterEach(async () => {
-//         await specHelper.cleanup();
-//     });
-//
-//     afterAll(async () => {
-//         await queue.end();
-//         await specHelper.disconnect();
-//     });
-//
-//     describe('jobLock', () => {
-//         test('will not lock jobs since arg objects are different', async () => {
-//             worker1 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, jobs);
-//             worker2 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, jobs);
-//
-//             worker1.on('error', (error) => { throw error; });
-//             worker2.on('error', (error) => { throw error; });
-//
-//             await worker1.connect();
-//             await worker2.connect();
-//
-//             await new Promise((resolve) => {
-//                 const startTime = new Date().getTime();
-//                 let completed = 0;
-//
-//                 const onComplete = function (q, job, result) {
-//                     completed++;
-//                     if (completed === 2) {
-//                         worker1.end();
-//                         worker2.end();
-//                         expect(new Date().getTime() - startTime).toBeLessThan(jobDelay * 2);
-//                         resolve();
-//                     }
-//                 };
-//
-//                 worker1.on('success', onComplete);
-//                 worker2.on('success', onComplete);
-//
-//                 queue.enqueue(specHelper.queue, 'slowAdd', [{ name: 'Walter White' }, 2]);
-//                 queue.enqueue(specHelper.queue, 'slowAdd', [{ name: 'Jesse Pinkman' }, 2]);
-//
-//                 worker1.start();
-//                 worker2.start();
-//             });
-//         });
-//
-//         test('allows the key to be specified as a function', async () => {
-//             let calls = 0;
-//
-//             await new Promise(async (resolve) => {
-//                 const functionJobs = {
-//                     jobLockAdd: {
-//                         plugins: ['JobLock'],
-//                         pluginOptions: {
-//                             JobLock: {
-//                                 key() {
-//                                     // Once to create, once to delete
-//                                     if (++calls === 2) {
-//                                         worker1.end();
-//                                         resolve();
-//                                     }
-//                                     const key = this.worker.connection.key('customKey', Math.max.apply(Math.max, this.args));
-//
-//                                     return key;
-//                                 }
-//                             }
-//                         },
-//                         perform: (a, b) => a + b
-//                     }
-//                 };
-//
-//                 worker1 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, functionJobs);
-//                 worker1.on('error', (error) => { throw error; });
-//                 await worker1.connect();
-//                 await queue.enqueue(specHelper.queue, 'jobLockAdd', [1, 2]);
-//                 worker1.start();
-//             });
-//         });
-//
-//         test('will not run 2 jobs with the same args at the same time', async () => {
-//             let count = 0;
-//
-//             worker1 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, jobs);
-//             worker2 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, jobs);
-//
-//             worker1.on('error', (error) => { throw error; });
-//             worker2.on('error', (error) => { throw error; });
-//
-//             await new Promise(async (resolve) => {
-//                 await worker1.connect();
-//                 await worker2.connect();
-//
-//                 const onComplete = async () => {
-//                     count++;
-//                     expect(count).toBe(1);
-//                     worker1.end();
-//                     worker2.end();
-//
-//                     const timestamps = await queue.timestamps();
-//                     let dealyedJob = await specHelper.redis.lpop(`${specHelper.namespace}:delayed:${Math.round(timestamps[0] / 1000)}`);
-//
-//                     expect(dealyedJob).toBeDefined();
-//                     dealyedJob = JSON.parse(dealyedJob);
-//                     expect(dealyedJob.class).toBe('slowAdd');
-//                     expect(dealyedJob.args).toEqual([1, 2]);
-//
-//                     resolve();
-//                 };
-//
-//                 worker1.on('success', onComplete);
-//                 worker2.on('success', onComplete);
-//
-//                 await queue.enqueue(specHelper.queue, 'slowAdd', [1, 2]);
-//                 await queue.enqueue(specHelper.queue, 'slowAdd', [1, 2]);
-//
-//                 worker1.start();
-//                 worker2.start();
-//             });
-//         });
-//
-//         test('will run 2 jobs with the different args at the same time', async () => {
-//             worker1 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, jobs);
-//             worker2 = new NodeResque.Worker({ connection: specHelper.cleanConnectionDetails(), timeout: specHelper.timeout, queues: specHelper.queue }, jobs);
-//
-//             worker1.on('error', (error) => { throw error; });
-//             worker2.on('error', (error) => { throw error; });
-//
-//             await worker1.connect();
-//             await worker2.connect();
-//
-//             const startTime = new Date().getTime();
-//             let completed = 0;
-//
-//             await new Promise(async (resolve) => {
-//                 const onComplete = function (q, job, result) {
-//                     completed++;
-//                     if (completed === 2) {
-//                         worker1.end();
-//                         worker2.end();
-//                         const delta = (new Date().getTime() - startTime);
-//
-//                         expect(delta).toBeLessThan(jobDelay * 2);
-//                         resolve();
-//                     }
-//                 };
-//
-//                 worker1.on('success', onComplete);
-//                 worker2.on('success', onComplete);
-//
-//                 await queue.enqueue(specHelper.queue, 'slowAdd', [1, 2]);
-//                 await queue.enqueue(specHelper.queue, 'slowAdd', [3, 4]);
-//
-//                 worker1.start();
-//                 worker2.start();
-//             });
-//         });
-//     });
-// });
+'use strict';
+
+const assert = require('chai').assert;
+const mockery = require('mockery');
+const sinon = require('sinon');
+
+sinon.assert.expose(assert, { prefix: '' });
+
+describe('Plugin Test', () => {
+    let mockRedis;
+    let BlockedBy;
+    let blockedBy;
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+    });
+
+    beforeEach(() => {
+        mockRedis = sinon.stub().returns({
+            set: sinon.stub().resolves()
+        });
+
+        mockery.registerMock('ioredis', mockRedis);
+
+        // eslint-disable-next-line global-require
+        BlockedBy = require('../lib/BlockedBy.js');
+
+        blockedBy = new BlockedBy();
+    });
+
+    afterEach(() => {
+        mockery.deregisterAll();
+        mockery.resetCache();
+    });
+
+    after(() => {
+        mockery.disable();
+    });
+
+    describe('jobLock', () => {
+        it('set the current jobid if not blocked', async () => {
+            await blockedBy.beforePerform({ buildId: 1234, jobId: 777 }, (err, result) => {
+                assert.isNull(err);
+                assert.isNull(result);
+                assert.calledWith(mockRedis.set, '777');
+            });
+        });
+    });
+});
